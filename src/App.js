729179b839fe14e4b47eff900537dc4d1c37f733
edwardfgthomas@gmail.com <edwardfgthomas@gmail.com>
2023-03-03 12:33:18 +0000

    create first component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import "./App.css";
+import Title from './components/Title';
 
 function App() {
+  console.log('Test console log');
+  const testVariable = true;
   return (
     <div className="App">
-      <header className="App-header">
-
-        <h1 id = "navbar">Chitter</h1>
+      {testVariable ?
+      <>
+        <Title/>
         <p>hello</p>
-      </header>
+      </>
+      :
+      <p>You cannot see the test</p>
+    }
+
 
       
     </div>
---
+++ src/components/Title.js
@@ -0,0 +1,5 @@
+function Title() {
+    return <h1>Chitter</h1>;
+}
+
+export default Title;
