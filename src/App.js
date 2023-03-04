e232a4706bab65564fae65a8a82aeab8d38777ed
edwardfgthomas@gmail.com <edwardfgthomas@gmail.com>
2023-03-04 21:12:35 +0000

    add tailwind css and test array of objects

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
-import "./App.css";
-import Title from './components/Title';
+import "./index.css";
+import Employee from './components/Employee';
 import { useState } from "react";
 
 function App() {
   const [role, setRole] = useState('dev')
+  const [employees, setEmployees] = useState([
+    {
+      id: 1,
+      name: "Edward",
+     role: "CEO",
+      img: "https://www.gatesfoundation.org/-/media/gfo/3about/3people/ga_warren_buffett_20210203_0001.jpg"
+    },
+    {
+      id: 2,
+      name: "Edward",
+     role: "CEO",
+      img: "https://www.gatesfoundation.org/-/media/gfo/3about/3people/ga_warren_buffett_20210203_0001.jpg"
+    },
+    {
+      id: 3,
+      name: "Edward",
+     role: "CEO",
+      img: "https://www.gatesfoundation.org/-/media/gfo/3about/3people/ga_warren_buffett_20210203_0001.jpg"
+    },
+    {
+      id: 4,
+      name: "Edward",
+     role: "CEO",
+      img: "https://www.gatesfoundation.org/-/media/gfo/3about/3people/ga_warren_buffett_20210203_0001.jpg"
+    },
+    {
+      id: 5,
+      name: "Edward",
+     role: "CEO",
+      img: "https://www.gatesfoundation.org/-/media/gfo/3about/3people/ga_warren_buffett_20210203_0001.jpg"
+    },
+    {
+      id: 6,
+      name: "Edward",
+     role: "CEO",
+      img: "https://www.gatesfoundation.org/-/media/gfo/3about/3people/ga_warren_buffett_20210203_0001.jpg"
+    }
+  ])
   const testVariable = true;
   return (
     <div className="App">
@@ -13,9 +51,18 @@ function App() {
         console.log(e.target.value);
         setRole(e.target.value);
       }}/>
-        <Title name="Chitter" role="user"/>
-        <Title name="Twitter"/>
-        <Title name="Facebook" role={role}/>
+
+      <div className="flex flex-wrap justify-center">
+       {employees.map((employee) => {
+        return (
+        <Employee 
+          id ={employee.id}
+          name={employee.name} 
+          role={employee.role} 
+          img={employee.img} />
+          );
+       })}
+      </div>
       </>
       :
       <p>You cannot see the test</p>
