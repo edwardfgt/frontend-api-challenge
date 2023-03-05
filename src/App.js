5b45ebe29f8960a05507b6268a570047775ae864
edwardfgthomas@gmail.com <edwardfgthomas@gmail.com>
2023-03-05 20:31:18 +0000

    display peeps

--- src/App.js
+++ src/App.js
@@ -1,89 +1,48 @@
 import "./index.css";
-import Employee from './components/Employee';
 import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
-  const [role, setRole] = useState('dev')
-  const [employees, setEmployees] = useState([
-    {
-      id: 1,
-      name: "Edward",
-      role: "CEO",
-      img: "https://www.gatesfoundation.org/-/media/gfo/3about/3people/ga_warren_buffett_20210203_0001.jpg"
-    },
-    {
-      id: 2,
-      name: "Edward",
-      role: "CEO",
-      img: "https://www.gatesfoundation.org/-/media/gfo/3about/3people/ga_warren_buffett_20210203_0001.jpg"
-    },
-    {
-      id: 3,
-      name: "Edward",
-      role: "CEO",
-      img: "https://www.gatesfoundation.org/-/media/gfo/3about/3people/ga_warren_buffett_20210203_0001.jpg"
-    },
-    {
-      id: 4,
-      name: "Edward",
-      role: "CEO",
-      img: "https://www.gatesfoundation.org/-/media/gfo/3about/3people/ga_warren_buffett_20210203_0001.jpg"
-    },
-    {
-      id: 5,
-      name: "Edward",
-      role: "CEO",
-      img: "https://www.gatesfoundation.org/-/media/gfo/3about/3people/ga_warren_buffett_20210203_0001.jpg"
-    },
-    {
-      id: 6,
-      name: "Edward",
-      role: "CEO",
-      img: "https://www.gatesfoundation.org/-/media/gfo/3about/3people/ga_warren_buffett_20210203_0001.jpg"
+    const [post, setPost] = useState([]);
+
+    const [token, setToken] = useState("_2a_12_uDNKJIWzdHBVKJrBL1dsQO");
+    const [id, setId] = useState("1269");
+    const [body, setBody] = useState("");
+    
+
+    useEffect(() => {
+      posts()
+    }, [])
+    
+
+    const posts = async () => {
+        const response = await fetch('https://chitter-backend-api-v2.herokuapp.com/peeps');
+        const data = await response.json();
+        console.log(data);
+        setPost(data);
     }
-  ])
-
-  function updateEmployee(id, newName, newRole){
-    const updatedEmployees = employees.map((employee) => {
-      if (id === employee.id){
-        return {...employee, name: newName, role: newRole }
-      } else {
-        return employee
-      }
-    });
-    setEmployees(updatedEmployees);
-  }
-
-  const showEmployees = true;
-  return (
-    <div className="App">
-      {showEmployees ?
-      <>
-      <input type='text' onChange={(e) => {
-        console.log(e.target.value);
-        setRole(e.target.value);
-      }}/>
-
-      <div className="flex flex-wrap justify-center">
-       {employees.map((employee) => {
+    return <div className="app">
+    <div id="posts">
+    {post.map((data) => {
         return (
-        <Employee 
-          key={employee.id}
-          id ={employee.id}
-          name={employee.name} 
-          role={employee.role} 
-          img={employee.img}
-          updateEmployee={updateEmployee}
-          />
-          );
-       })}
-      </div>
-      </>
-      :
-      <p>You cannot see the test</p>
-    }
-    </div>
-  );
-}
+            <div id="divOutline" className="row-span-1 m-3 py-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 w-full content-center">
+                <div id="textbox" className="text-center space-y-2 sm:text-left">
+                    <p>{data.body}</p>
+                    <p>{data.user.handle}</p>
+                </div>
+            </div>
+        )
+    })}
 
+    </div>
+    
+    </div>
+}
 export default App;
+
+
+// curl "https://chitter-backend-api-v2.herokuapp.com/peeps" \
+//   -X POST \
+//   -H "Authorization: Token token=_2a_12_uDNKJIWzdHBVKJrBL1dsQO" \
+//   -H "Content-Type: application/json" \
+//   -d '{"peep": {"user_id":1269, "body":"Does this thing work?"}}'
